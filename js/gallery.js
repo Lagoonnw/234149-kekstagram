95c858d751cb6aea6e23699be67a169f74e36c53
Ekaterina Krutakova <lagoonnw@gmail.com>
2017-04-21 21:14:51 +0500

    задание перламутровые пуговицы

--- js/gallery.js
+++ js/gallery.js
@@ -7,6 +7,8 @@
   var fragment = document.createDocumentFragment();
   var uploadForm = document.getElementById('upload-select-image');
   var cropOverlay = document.querySelector('.upload-overlay');
+  var galleryFiltersContainer = document.querySelector('.filters');
+  var pictures;
 
   var openGallery = function () {
     galleryOverlay.classList.remove('invisible');
@@ -31,13 +33,39 @@
   };
 
   var onLoad = function (data) {
-    showPhotos(data);
+    pictures = data;
+    showPhotos(pictures);
+    return pictures;
   };
 
+  var onGalleryFiltersClick = function (evt) {
+    if (evt.target.hasAttribute('type')) {
+      var galleryFilter = evt.target;
+      switch (galleryFilter.value) {
+        case 'new':
+          window.utils.debounce(function () {
+            showNewPhotos(pictures);
+          });
+          break;
+        case 'popular':
+          window.utils.debounce(function () {
+            showPopularPhotos(pictures);
+          });
+          break;
+        case 'discussed':
+          window.utils.debounce(function () {
+            showtDiscussedPhotos(pictures);
+          });
+          break;
+      }
+    }
+  };
 
   window.load(URL, onLoad);
   uploadForm.classList.remove('invisible');
   cropOverlay.classList.add('invisible');
+  galleryFiltersContainer.classList.remove('hidden');
+  galleryFiltersContainer.addEventListener('click', onGalleryFiltersClick);
 
   function showPhotos(photos) {
     photos.forEach(function (arrayItem, i) {
@@ -54,4 +82,30 @@
     picturesBlock.appendChild(fragment);
   }
 
+  function removePictures(container) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+
+  function showPopularPhotos(photos) {
+    var _photos = photos;
+    removePictures(picturesBlock);
+    showPhotos(_photos);
+  }
+
+  function showNewPhotos(photos) {
+    var _photos = photos.sort(window.utils.sortRandom).slice(0, 10);
+    removePictures(picturesBlock);
+    showPhotos(_photos);
+  }
+
+  function showtDiscussedPhotos(photos) {
+    var sortCompare = function (a, b) {
+      return b.comments.length - a.comments.length;
+    };
+    var _photos = photos.sort(sortCompare).slice();
+    removePictures(picturesBlock);
+    showPhotos(_photos);
+  }
 })();
--- js/utils.js
+++ js/utils.js
@@ -3,6 +3,8 @@
 window.utils = (function () {
   var ESC_KEY_CODE = 27;
   var ENTER_KEY_CODE = 13;
+  var DEBOUNCE_INTERVAL = 500;
+  var lastTimeout;
 
   var getRandomArrayItem = function (array) {
     var randomNumber = Math.floor(Math.random() * array.length);
@@ -21,10 +23,23 @@ window.utils = (function () {
     return keyCode === ENTER_KEY_CODE;
   };
 
+  var sortRandom = function () {
+    return 0.5 - Math.random();
+  };
+
+  var debounce = function (cb) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(cb, DEBOUNCE_INTERVAL);
+  };
+
   return {
     getRandomArrayItem: getRandomArrayItem,
     getRandomNumber: getRandomNumber,
     isEsc: isEscKey,
-    isEnter: isEnterKey
+    isEnter: isEnterKey,
+    sortRandom: sortRandom,
+    debounce: debounce
   };
 })();
